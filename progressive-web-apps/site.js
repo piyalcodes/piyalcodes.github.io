b8afefa3e76a8188bc891452777cae7569a1c4a3
Piyal <tenamax@gmail.com>
2018-06-01 15:32:36 +0530

    changes

--- progressive-web-apps/site.js
+++ progressive-web-apps/site.js
@@ -10,9 +10,7 @@ if (ServiceWorker) {
 }
 
 window.onload = function () {
-   alert('1')
    if (!navigator.onLine) {
-      alert('2')
       console.log("You are getting an offline content.");
       console.log(document.getElementById('offlineMsg'));
       if (document.getElementById('offlineMsg')) {
